c93a9ee80d42c9be1a7154167f65547f2a5f9d25
Arnab Kumar Goswami <arnab@arnab.goswami.linux.pc>
2020-07-10 19:02:45 +0530

    Added jodit with image upload functionality

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import BlogPage from "./pages/blog_page/components/BlogPage";
+
+function App() {
+  return (
+    <div className="App">
+      <BlogPage />
+    </div>
+  );
+}
+
+export default App;
